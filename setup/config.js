4810ccd922c1ea463ca344ebfd2bb6dc8dfbeafd
Sushant <sushantshinde5055@gmail.com>
2021-08-09 10:19:06 +0530

    Hostname changed for heroku

--- setup/config.js
+++ setup/config.js
@@ -1,7 +1,7 @@
 const myconfig={
     mongoURL:`mongodb+srv://${process.env.mDB_USER}:${process.env.mDB_PASS}@addressbook.mvxhm.mongodb.net/${process.env.mDB_NAME}?retryWrites=true&w=majority`,
     nodePort:3000,
-    hostname:'127.0.0.1'
+    hostname:'0.0.0.0'
 }
 
 module.exports=myconfig;
